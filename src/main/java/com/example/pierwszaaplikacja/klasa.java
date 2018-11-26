9c839ff6acad39bb5e212035fd51f3ef9577af27
student <student@student>
2018-11-26 17:13:30 +0100

    Add basic frontent app

--- src/main/java/com/example/pierwszaaplikacja/klasa.java
+++ src/main/java/com/example/pierwszaaplikacja/klasa.java
@@ -1,6 +1,9 @@
 package com.example.pierwszaaplikacja;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class klasa {
+    @GetMapping (path ="api/hello")
     public String hello() {
         return "hello world from the server";
     }
